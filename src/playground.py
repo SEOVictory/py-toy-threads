e99aaa86a942f0595ae590286b14ed939013f797
vic <git@dfhu.org>
2014-03-17 19:03:53 -0400

    a stoppable while 1 spinner

--- src/playground.py
+++ src/playground.py
@@ -1,6 +1,14 @@
+from time import sleep
+
 from toys.whrrrler import Whrrrler
 from toys.spinner import Spinner
 
+
 if __name__ == '__main__':
+
+    spinner1 = Spinner(1, 'spin-1', 1)
+    spinner1.start()
+    sleep(2)
+    spinner1.stop()
+
     whrrrler = Whrrrler()
-    spinner = Spinner()
--- src/toys/spinner.py
+++ src/toys/spinner.py
@@ -1,11 +1,26 @@
 from time import sleep
 
+from threading import Thread
 
-class Spinner(object):
-    play_on = False
 
-    def play(self):
+class Spinner(Thread):
+    play_on = True
+    thread_id = None
+    name = None
+    counter = None
+
+    def __init__(self, thread_id, name, counter):
+        super(Spinner, self).__init__()
+        self.thread_id = thread_id
+        self.name = name
+        self.counter = counter
+
+    def run(self):
+        print "Starting to play"
 
         while self.play_on:
             sleep(.5)
-            print 'Spin!'
+            print 'Spin!', self.thread_id, self.name, self.counter
+
+    def stop(self):
+        self.play_on = False
